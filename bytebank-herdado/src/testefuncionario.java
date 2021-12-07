91d6ed3158082d46cb2e44ca8f3235bf487755bb
Celio Fratari H Neto <93385991+celiofratari@users.noreply.github.com>
2021-12-07 09:59:06 -0300

    Update testefuncionario.java

--- bytebank-herdado/src/testefuncionario.java
+++ bytebank-herdado/src/testefuncionario.java
@@ -1,16 +1,16 @@
 
-public class testefuncionario {
+public class TesteFuncionario {
 
 	public static void main(String[] args) {
 		
-	    funcionario nico = new funcionario();
-	    nico.setNome("nico");
-	    nico.setCpf("070757036-09");
-	    nico.setSalario(2590.00);
+	    Funcionario Nico = new Funcionario();
+	    Nico.setNome("nico");
+	    Nico.setCPF("070757036-09");
+	    Nico.setSalario(2590.00);
 	    
-	    System.out.println(nico.getNome());
-	    System.out.println(nico.getCpf());
-	    System.out.println(nico.getbonificacao());
+	    System.out.println(Nico.getNome());
+	    System.out.println(Nico.getCPF());
+	    System.out.println(Nico.getBonificacao());
 	    
     }
 }
