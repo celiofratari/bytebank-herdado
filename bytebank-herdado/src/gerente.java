0754ac9e67521af75e4c8027ce4fdb0885390bc1
Celio Fratari H Neto <93385991+celiofratari@users.noreply.github.com>
2021-12-07 09:55:26 -0300

    Update gerente.java

--- bytebank-herdado/src/gerente.java
+++ bytebank-herdado/src/gerente.java
@@ -1,22 +1,22 @@
 
-public class gerente extends funcionario {
+public class Gerente extends Funcionario {
   
-    private int senha;
+    private int Senha;
     
-    public void setSenha(int senha) {
-		this.senha = senha;
+    public void setSenha(int Senha) {
+		this.Senha = Senha;
 	}
     
-    public boolean autentica(int senha) {
-    	if(this.senha == senha) {
+    public boolean Autentica(int Senha) {
+    	if(this.Senha == Senha) {
     		return true;
     	}else {
     		return false;
     	}
     }
     
-    public double getbonificacao() {   
-	    return super.getbonificacao() + super.getSalario(); 
+    public double getBonificacao() {   
+	    return super.getBonificacao() + super.getSalario(); 
     }
     
 
